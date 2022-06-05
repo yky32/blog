3c3fcba8ffc949c8002beb69dcae8f30d9784494
Wayne <yky32is@gmail.com>
2022-06-05 19:42:24 +0800

    Add image and section

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Format from '../layout/format'
 
+//components
+import Section1 from '../components/section1'
+
 export default function Home() {
   return (
     <Format>
-      <h1>Main Content</h1>
+      <Section1></Section1>
     </Format>
   )
 }
