137fef90e3e482771aa5e74e0c94504cc766895e
Wayne <yky32is@gmail.com>
2022-06-08 01:01:18 +0800

    Add Sliders and Author

--- components/section1.js
+++ components/section1.js
@@ -1,16 +1,41 @@
 import Image from "next/image"
 import Link from "next/link"
+import Author from "./_child/author"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, {Autoplay} from 'swiper'
+import 'swiper/css'
 
 export default function section1() {
+  SwiperCore.use([Autoplay])
+
+  const bg = {
+    background: "url('/images/banner.png') no-repeat",
+    backgroundPosition: "right"
+  }
+
   return (
-    <section className="py-16">
+    <section className="py-16" style={bg} >
       <div className="container mx-auto md:px-20">
         <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
-        {Slide()}
+
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 2000
+          }}
+        >
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+    
+        </Swiper>
       </div>
     </section>
   )
 }
+
 
 function Slide() {
   return (
@@ -24,13 +49,13 @@ function Slide() {
           <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
         </div>
         <div className="title">
-            <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
+          <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
         </div>
         <p className="text-gray-500 py-3">
-            Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-            text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+          Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
+          text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
         </p>
-        <h1>author</h1>
+        <Author></Author>
       </div>
     </div>
   )
