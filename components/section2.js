99031c38e187ebba19c2ffc273eea7dd073a9d9b
wayneyu <yky32is@gmail.com>
2022-06-13 01:01:52 +0800

    Add Section 2

---
+++ components/section2.js
@@ -0,0 +1,12 @@
+export default function section2() {
+	return (
+		<section className="container mx-auto md:px-20 py-10">
+			<h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
+
+			{/*grid columns*/}
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
+				
+			</div>
+		</section>
+	)
+}
