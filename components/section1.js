0aeab4c9db2c4bb03a3ae8a7d874a0869bf4fdf3
Wayne <yky32is@gmail.com>
2022-06-13 01:59:38 +0800

    Add alt

--- components/section1.js
+++ components/section1.js
@@ -16,7 +16,7 @@ export default function section1() {
   return (
     <section className="py-16" style={bg} >
       <div className="container mx-auto md:px-20">
-        <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
+        <h1 className="font-bold text-4xl pb-12 text-center">Trendings</h1>
 
         <Swiper
           slidesPerView={1}
