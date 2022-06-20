ca7825fb8b39dad51788339ed0b19479e3be00c5
Wayne Yu <wayneyu.dev@gmail.com>
2022-06-21 02:26:37 +0800

    Add getPosts api.

--- components/section2.js
+++ components/section2.js
@@ -1,8 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 import Author from "./_child/author"
+import getPost from "../lib/helper"
 
 export default function section2() {
+
+	getPost(2).then(res => console.log(res))
+
 	return (
 		<section className="container mx-auto md:px-20 py-10">
 			<h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
