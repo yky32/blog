564692214fa8c73bed4fef6b58ddc168cf728d4e
Wayne <yky32is@gmail.com>
2022-06-23 02:13:08 +0800

    All data are fetched from API

--- components/section1.js
+++ components/section1.js
@@ -1,62 +1,77 @@
 import Image from "next/image"
 import Link from "next/link"
 import Author from "./_child/author"
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, {Autoplay} from 'swiper'
-import 'swiper/css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
+import fetcher from '../lib/fetcher'
+import Spinner from "./_child/spinner"
+import Error from "./_child/error"
+
 
 export default function section1() {
-  SwiperCore.use([Autoplay])
 
-  const bg = {
-    background: "url('/images/banner.png') no-repeat",
-    backgroundPosition: "right"
-  }
+    const { data, isLoading, isError } = fetcher('api/trending')
+    
+    if(isLoading) return <Spinner></Spinner>;
+    if(isError) return <Error></Error>
+
+    SwiperCore.use([Autoplay])
+
+    const bg = {
+        background: "url('/images/banner.png') no-repeat",
+        backgroundPosition: "right"
+    }
 
   return (
-    <section className="py-16" style={bg} >
-      <div className="container mx-auto md:px-20">
-        <h1 className="font-bold text-4xl pb-12 text-center">Trendings</h1>
+    <section className="py-16" style={bg}>
+        <div className="container mx-auto md:px-20">
+            <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
 
-        <Swiper
-          slidesPerView={1}
-          loop={true}
-          autoplay={{
-            delay: 2000
-          }}
-        >
-          <SwiperSlide>{Slide()}</SwiperSlide>
-          <SwiperSlide>{Slide()}</SwiperSlide>
-          <SwiperSlide>{Slide()}</SwiperSlide>
-          <SwiperSlide>{Slide()}</SwiperSlide>
-    
-        </Swiper>
-      </div>
+            <Swiper
+                slidesPerView={1}
+                // loop={true}
+                // autoplay= {{
+                //     delay: 2000
+                // }}
+                >
+                {
+                    data.map((value, index) => (
+                        <SwiperSlide key={index}><Slide data={value}></Slide></SwiperSlide>
+                    ))
+                }
+            </Swiper>
+
+            
+        </div>
     </section>
   )
 }
 
+function Slide({ data }){
 
-function Slide() {
-  return (
-    <div className="grid md:grid-cols-2">
-      <div className="image">
-        <Link href={"/"}><a><Image alt="" src={"/images/img1.jpg"} width={600} height={600} /></a></Link>
-      </div>
-      <div className="info flex justify-center flex-col">
-        <div className="cat">
-          <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
-          <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
+    const { id, title, category, img, published, description ,author } = data;
+
+    return (
+        <div className="grid md:grid-cols-2">
+            <div className="image">
+                <Link href={"/"}><a><Image alt="" src={img || "/"} width={600} height={600} /></a></Link>
+            </div>
+            <div className="info flex justify-center flex-col">
+                <div className="cat">
+                    <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</a></Link>
+                    <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</a></Link>
+                </div>
+                <div className="title">
+                    <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">{title || "Unknown"}</a></Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                    {description || "description"}
+                </p>
+                { author ? <Author></Author> : <></>}
+            </div>
         </div>
-        <div className="title">
-          <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
-        </div>
-        <p className="text-gray-500 py-3">
-          Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
-        </p>
-        <Author></Author>
-      </div>
-    </div>
-  )
+    )
 }
--- components/section3.js
+++ components/section3.js
@@ -1,47 +1,60 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import Image from "next/image"
+import { Swiper, SwiperSlide } from "swiper/react"
 import Link from "next/link"
+import Image from "next/image"
 import Author from "./_child/author"
+import fetcher from '../lib/fetcher'
+import Spinner from "./_child/spinner"
+import Error from "./_child/error"
 
 export default function section3() {
+
+    const { data, isLoading, isError } = fetcher('api/popular')
+    
+    if(isLoading) return <Spinner></Spinner>;
+    if(isError) return <Error></Error>
+
   return (
     <section className="container mx-auto md:px-20 py-16">
-      <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
+        <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
 
+        {/* swiper */}
+        <Swiper
+            slidesPerView={2}
+        >
+            {
+                    data.map((value, index) => (
+                        <SwiperSlide key={index}><Post data={value}></Post></SwiperSlide>
+                    ))
+            }
+        </Swiper>
 
-      {/* swiper */}
-      <Swiper
-        slidesPerView={2}
-      >
-        <SwiperSlide>{Post()}</SwiperSlide>
-        <SwiperSlide>{Post()}</SwiperSlide>
-        <SwiperSlide>{Post()}</SwiperSlide>
-        <SwiperSlide>{Post()}</SwiperSlide>
-      </Swiper>
     </section>
   )
 }
 
-function Post() {
-  return (
-    <div className="grid">
-      <div className="images">
-        <Link href={"/"}><a><Image alt="" src={"/images/img1.jpg"} width={600} height={400} /></a></Link>
-      </div>
-      <div className="info flex justify-center flex-col py-4">
-        <div className="cat">
-          <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
-          <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
+
+function Post({ data }){
+
+    const { id, title, category, img, description, published, author } = data;
+
+    return (
+        <div className="grid">
+            <div className="images">
+                <Link href={"/"}><a><Image src={img || ""} width={600} height={400} /></a></Link>
+            </div>
+            <div className="info flex justify-center flex-col py-4">
+                <div className="cat">
+                    <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">{category || "No Category"}</a></Link>
+                    <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- {published || ""}</a></Link>
+                </div>
+                <div className="title">
+                    <Link href={"/"}><a className="text-3xl md:text-4xl font-bold text-gray-800 hover:text-gray-600">{title || "No Title"}</a></Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                {description || "No Description"}
+                </p>
+                { author ? <Author></Author> : <></>}
+            </div>
         </div>
-        <div className="title">
-          <Link href={"/"}><a className="text-3xl md:text-4xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
-        </div>
-        <p className="text-gray-500 py-3">
-          Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
-        </p>
-        <Author></Author>
-      </div>
-    </div>
-  )
-} 
+    )
+}
