bff3707edf936790f3533bf240024cd16f7903c1
wayne.yu <wayne.yu@eqonex.com>
2022-09-05 16:28:35 +0800

    Add md

---
+++ pages/mds/[slug].js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import md from 'markdown-it';
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync('mds/posts');
+    const paths = files.map((fileName) => ({
+        params: {
+            slug: fileName.replace('.md', ''),
+        },
+    }));
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+    const fileName = fs.readFileSync(`mds/posts/${slug}.md`, 'utf-8');
+    const { data: frontmatter, content } = matter(fileName);
+
+    console.log(frontmatter)
+    return {
+        props: {
+            frontmatter: frontmatter,
+            content,
+        },
+    };
+}
+
+export default function PostPage({ frontmatter: frontmatter, content }) {
+    return (
+        <div className='prose mx-auto'>
+            <h1>{frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+        </div>
+    );
+}
