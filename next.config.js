e2df64a2a4ecd0df4b85691e0b05c040da8d6a2f
wayne.yu <wayne.yu@eqonex.com>
2022-09-05 17:41:41 +0800

    Remove redirect

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_URL: process.env.API_URL,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/prism',
-        permanent: true
-      }
-    ];
   }
 }
 
